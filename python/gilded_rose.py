7f0daa8027e6945eaa5e88b4cee43b76865b83c6
Ricardo <rpenalozav@unal.edu.co>
2022-03-29 10:07:08 -0500

    refactor semantic Python

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,41 +1,4 @@
 # -*- coding: utf-8 -*-
-
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
-
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -44,3 +7,77 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class GildedRose(object):
+
+    AGED_BRIE:str = "Aged Brie"
+    BACKSTAGE_PASSES:str = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS:str = "Sulfuras, Hand of Ragnaros"
+
+    AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD:int = 0
+
+    BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD:int = 10
+    BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD:int = 5
+    BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD:int = 0
+
+    DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD:int = 0
+
+    MAX_QUALITY:str = 50
+    MIN_QUALITY:str = 0
+
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+
+            if item.name == GildedRose.AGED_BRIE:
+                self.decrease_sell_in(item)
+                self.update_aged_brie_quality(item)
+
+            elif item.name == GildedRose.BACKSTAGE_PASSES:
+                self.decrease_sell_in(item)
+                self.update_backstage_passes_quality(item)
+            elif item.name == GildedRose.SULFURAS:
+                break
+
+            else:
+                self.decrease_sell_in(item)
+                self.update_default_item_quality(item)
+                
+    def decrease_sell_in(self, item:Item)->None:
+        item.sell_in -= 1
+
+    def update_aged_brie_quality(self, item:Item)->None:
+        self.increase_quality(item)
+        if item.sell_in < GildedRose.AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD:
+            increase_quality(item)
+
+    def update_backstage_passes_quality(self, item:Item)->None:
+        self.increase_quality(item)
+        if item.sell_in < GildedRose.BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD:
+            self.increase_quality(item)
+        if item.sell_in < GildedRose.BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD:
+            self.increase_quality(item)
+        if item.sell_in < GildedRose.BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD:
+            self.reset_quality(item)
+
+    def update_default_item_quality(self, item:Item):
+        self.decrease_quality(item)
+        if item.sell_in < GildedRose.DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD:
+            self.decrease_quality(item)
+
+    def reset_quality(self, item:Item)->None:
+        item.quality=0
+
+    def increase_quality(self, item:Item)->None:
+        if item.quality < GildedRose.MAX_QUALITY:
+            item.quality += 1
+
+    def decrease_quality(self, item:Item)->None:
+        if item.quality < GildedRose.MIN_QUALITY:
+            item.quality -= 1
+        
+
+
